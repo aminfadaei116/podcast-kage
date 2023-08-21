adac5f022bd9a2d6900aa66dbde166d5f15984aa
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2023-08-21 13:31:08 -0400

    Update podcast_frontend.py

--- upload/main/podcast_frontend.py
+++ upload/main/podcast_frontend.py
@@ -51,7 +51,7 @@ def main():
 
         # Display the five key moments
         st.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']["choices"]
+        key_moments = podcast_info['podcast_highlights']["choices"][0]["message"]["content"]
         st.write(key_moments)
         # for moment in key_moments.split('\n'):
         #     st.markdown(
