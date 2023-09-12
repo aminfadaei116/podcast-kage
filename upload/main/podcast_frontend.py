9a82fbecce63136ef63fc043362329ff73768751
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2023-09-11 20:02:00 -0400

    Update podcast_frontend.py

--- upload/main/podcast_frontend.py
+++ upload/main/podcast_frontend.py
@@ -54,9 +54,9 @@ def main():
         st.subheader("Key Moments")
         key_moments = podcast_info['podcast_highlights']["choices"][0]["message"]["content"]
         st.write(key_moments)
-        # for moment in key_moments.split('\n'):
-        #     st.markdown(
-        #         f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+        for moment in key_moments.split('\n'):
+            st.markdown(
+                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
 
     # User Input box
     st.sidebar.subheader("Add and Process New Podcast Feed")
@@ -65,46 +65,46 @@ def main():
     process_button = st.sidebar.button("Process Podcast Feed")
     st.sidebar.markdown("**Note**: Podcast processing can take upto 5 mins, please be patient.")
 
-#     if process_button:
+    if process_button:
 
-#         # Call the function to process the URLs and retrieve podcast guest information
-#         podcast_info = process_podcast_info(url)
+        # Call the function to process the URLs and retrieve podcast guest information
+        podcast_info = process_podcast_info(url)
 
-#         # Right section - Newsletter content
-#         st.header("Newsletter Content")
+        # Right section - Newsletter content
+        st.header("Newsletter Content")
 
-#         # Display the podcast title
-#         st.subheader("Episode Title")
-#         st.write(podcast_info['podcast_details']['episode_title'])
+        # Display the podcast title
+        st.subheader("Episode Title")
+        st.write(podcast_info['podcast_details']['episode_title'])
 
-#         # Display the podcast summary and the cover image in a side-by-side layout
-#         col1, col2 = st.columns([7, 3])
+        # Display the podcast summary and the cover image in a side-by-side layout
+        col1, col2 = st.columns([7, 3])
 
-#         with col1:
-#             # Display the podcast episode summary
-#             st.subheader("Podcast Episode Summary")
-#             st.write(podcast_info['podcast_summary'])
+        with col1:
+            # Display the podcast episode summary
+            st.subheader("Podcast Episode Summary")
+            st.write(podcast_info['podcast_summary'])
 
-#         with col2:
-#             st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
+        with col2:
+            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
 
-#         # Display the podcast guest and their details in a side-by-side layout
-#         col3, col4 = st.columns([3, 7])
+        # Display the podcast guest and their details in a side-by-side layout
+        col3, col4 = st.columns([3, 7])
 
-#         with col3:
-#             st.subheader("Podcast Guest")
-#             st.write(podcast_info['podcast_guest']['name'])
+        with col3:
+            st.subheader("Podcast Guest")
+            st.write(podcast_info['podcast_guest']['name'])
 
-#         with col4:
-#             st.subheader("Podcast Guest Details")
-#             st.write(podcast_info["podcast_guest"]['summary'])
+        with col4:
+            st.subheader("Podcast Guest Details")
+            st.write(podcast_info["podcast_guest"]['summary'])
 
-#         # Display the five key moments
-#         st.subheader("Key Moments")
-#         key_moments = podcast_info['podcast_highlights']
-#         for moment in key_moments.split('\n'):
-#             st.markdown(
-#                 f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+        # Display the five key moments
+        st.subheader("Key Moments")
+        key_moments = podcast_info['podcast_highlights']
+        for moment in key_moments.split('\n'):
+            st.markdown(
+                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
 
 def create_dict_from_json_files(folder_path):
     json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
@@ -120,10 +120,10 @@ def create_dict_from_json_files(folder_path):
 
     return data_dict
 
-# def process_podcast_info(url):
-#     f = modal.Function.lookup("corise-podcast-project", "process_podcast")
-#     output = f.call(url, '/content/podcast/')
-#     return output
+def process_podcast_info(url):
+    f = modal.Function.lookup("corise-podcast-project", "process_podcast")
+    output = f.call(url, '/content/podcast/')
+    return output
 
 if __name__ == '__main__':
     main()
